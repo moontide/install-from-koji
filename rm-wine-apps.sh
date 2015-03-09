4e8b5b8a46c719e220dcb2840651d6dae638b0c3
LiuYan 刘研 <lovetide@qq.com>
2015-03-09 15:45:15 +0800

    从 1.7.35 开始，删除了 wine-wow 包，参见: http://koji.fedoraproject.org/koji/buildinfo?buildID=606266

--- rm-wine-apps.sh
+++ rm-wine-apps.sh
@@ -1,6 +1,6 @@
 # 参考
 # https://wiki.archlinux.org/index.php/Wine#Removing_menu_entries
 
-rm -v -f /usr/share/applications/wine-{wine{file,mine},notepad,wordpad}.desktop
+/usr/bin/rm -v -f /usr/share/applications/wine-{wine{file,mine},notepad,wordpad}.desktop
 
 #rm -v -f /home/liuyan/.local/share/{applications/wine-extension-*,icons/hicolor/*/*/application-x-wine-extension*,mime/packages/x-wine*,mime/application/x-wine-extension*}
--- wine.sh
+++ wine.sh
@@ -5,21 +5,21 @@ release="$2"
 arch=
 if [[ -z "$version" || -z "$release" ]]; then
 	echo "Usage: $0  <version>  <release>"
-	echo "eg: $0  1.7.17  1.fc20"
+	echo "eg: $0  1.7.35  2.fc21"
 	exit -1
 fi
 
-$dir/install-packages-from-koji.sh    p=$package,rpm=$package,v=$version,r=$release    rpm=$package-{alsa,capi,cms,core,desktop,ldap,openal,pulseaudio,twain,wow}    a=i686,rpm=$package-{capi,cms,core,ldap,openal,pulseaudio,twain}    a=noarch,rpm=$package-{common,courier-fonts,filesystem,fonts,fixedsys-fonts,marlett-fonts,ms-sans-serif-fonts,small-fonts,symbol-fonts,system-fonts,systemd,tahoma-fonts,wingdings-fonts}
+$dir/install-packages-from-koji.sh    p=$package,rpm=$package,v=$version,r=$release    rpm=$package-{alsa,capi,cms,core,ldap,openal,opencl,pulseaudio,twain}    a=i686,rpm=$package-{capi,cms,core,ldap,openal,opencl,pulseaudio,twain}    a=noarch,rpm=$package-{common,arial-fonts,courier-fonts,desktop,filesystem,fonts,fixedsys-fonts,marlett-fonts,ms-sans-serif-fonts,small-fonts,symbol-fonts,system-fonts,systemd,tahoma-fonts,tahoma-fonts-system,wingdings-fonts}
 
-# 去掉不需要的 wine 程序
-rm -f /usr/share/applications/wine-{notepad,winefile,winemine,wordpad}.desktop
+# 去掉不需要的 wine 程序菜单项
+/usr/bin/rm -v -f /usr/share/applications/wine-{notepad,winefile,winemine,wordpad}.desktop
 
 # 删除右键菜单条目
 # https://wiki.archlinux.org/index.php/wine#Removing_menu_entries
 user_dir=/home/liuyan
 #rm -f $user_dir/.local/share/applications/wine/Programs/*
-rm -f $user_dir/.local/share/applications/wine-extension*
-rm -f $user_dir/.local/share/icons/hicolor/*/*/application-x-wine-extension*
-rm -f $user_dir/.local/share/mime/application/x-wine-extension*
-rm -f $user_dir/.local/share/mime/packages/x-wine*
+/usr/bin/rm -v -f $user_dir/.local/share/applications/wine-extension*
+/usr/bin/rm -v -f $user_dir/.local/share/icons/hicolor/*/*/application-x-wine-extension*
+/usr/bin/rm -v -f $user_dir/.local/share/mime/application/x-wine-extension*
+/usr/bin/rm -v -f $user_dir/.local/share/mime/packages/x-wine*
 
