41dd46fcdd4ee7e128f2c584d74f92e50eb3a0ce
LiuYan 刘研 <lovetide@qq.com>
2021-06-16 12:02:40 +0800

    增加 pipewire

---
+++ pipewire.sh
@@ -0,0 +1,15 @@
+dir=$(dirname $0)
+package=pipewire
+version="$1"
+release="$2"
+arch=
+
+if [[ -z "$version" || -z "$release" ]]; then
+        echo "Usage: $0  <version>  <release>"
+        echo "eg: $0  0.3.29  1.fc34"
+        exit -1
+fi
+
+shift 2
+$dir/install-packages-from-koji.sh    p=$package,v=$version,r=$release,rpm=$package{,-{alsa,gstreamer,jack-audio-connection-kit,libs,plugin-jack,pulseaudio,utils}}    a=i686,rpm=$package-{libs,alsa}    $*
+
